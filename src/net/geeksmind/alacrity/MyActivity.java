9c6322a46df85d80c95197479e24daf01932b3ae
coderh <ren.hao.invictus@gmail.com>
2013-09-18 22:20:34 +0200

    persistent storage implemented focus shifting removed

--- src/net/geeksmind/alacrity/MyActivity.java
+++ src/net/geeksmind/alacrity/MyActivity.java
@@ -1,6 +1,7 @@
 package net.geeksmind.alacrity;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -12,28 +13,25 @@ import android.widget.*;
 public class MyActivity extends Activity {
 
     // Constants
-    private final static String CLEAR_IP = " . . . ";
-    private final static String DEFAULT_IP = "192.168.0.1";
+    private static final String CLEAR_IP = " . . . ";
+    private static final String DEFAULT_IP = "192.168.0.1";
+    public static final String PREFS_NAME = "PrefsFile";
+    public static final String DEFAULT_IP_ADDR_KEY = "defaultIpAddr";
+    public static final String OPENING_IP_ADDR_KEY = "openingIpAddr";
 
     // GUI Widgets
     private Button setAsDefaultButton;
     private Button emitButton;
-    private LinearLayout layoutIpAddr;
     private EditText edtTextIpAddrChunk1;
     private EditText edtTextIpAddrChunk2;
     private EditText edtTextIpAddrChunk3;
     private EditText edtTextIpAddrChunk4;
     private RadioGroup cmdOptionsRadioGroup;
 
-    //TODO: remove focus when buttons pushed
-
     //TODO: make ipChunkList manipulations generic
-
-    //TODO: persistent storage of default ipAddr
-
     //TODO: JSONObject and web communication
 
-    public void addTextCheckerToIpChunks(EditText... editTexts) {
+    public void addListenersToIpChunks(EditText... editTexts) {
         for (final EditText editText : editTexts) {
             editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
                 @Override
@@ -62,12 +60,6 @@ public class MyActivity extends Activity {
                         editText.setError("between 0 and 255");
                     } else {
                         editText.setError(null);
-                        Log.d("TextChange", editText.getText().toString());
-                        // shift focus to the next editText which is not the last one
-                        int edtViewIndex = layoutIpAddr.indexOfChild(editText);
-                        if (editText.getText().toString().length() == 3 && edtViewIndex < layoutIpAddr.getChildCount() - 1 && editText.isFocused()) {
-                            layoutIpAddr.getChildAt(edtViewIndex + 2).requestFocus();
-                        }
                     }
 
                     // detect GUI errors, e.g. any IpChunk invalid, then disable buttons
@@ -95,7 +87,6 @@ public class MyActivity extends Activity {
         setContentView(R.layout.main);
 
         emitButton = (Button) this.findViewById(R.id.buttonEmit);
-        layoutIpAddr = (LinearLayout) this.findViewById(R.id.linearLayoutIpChunkList);
         setAsDefaultButton = (Button) this.findViewById(R.id.buttonSetAsDefault);
         edtTextIpAddrChunk1 = (EditText) this.findViewById(R.id.editViewIpAddr1);
         edtTextIpAddrChunk2 = (EditText) this.findViewById(R.id.editViewIpAddr2);
@@ -106,7 +97,29 @@ public class MyActivity extends Activity {
         // make <light on> as default
         cmdOptionsRadioGroup.check(R.id.radioButtonOn);
 
-        addTextCheckerToIpChunks(edtTextIpAddrChunk1, edtTextIpAddrChunk2, edtTextIpAddrChunk3, edtTextIpAddrChunk4);
+        addListenersToIpChunks(edtTextIpAddrChunk1, edtTextIpAddrChunk2, edtTextIpAddrChunk3, edtTextIpAddrChunk4);
+        setIPAddrToGUI(getPrefIpAddr(OPENING_IP_ADDR_KEY));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        setPrefIpAddr(OPENING_IP_ADDR_KEY, getIPAddrFromGUI());
+    }
+
+    public String getPrefIpAddr(String prefKey) {
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        return settings.getString(prefKey, DEFAULT_IP);
+    }
+
+    public void setPrefIpAddr(String prefKey, String ip) {
+        // We need an Editor object to make preference changes.
+        // All objects are from android.context.Context
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(prefKey, ip);
+        // Commit the edits!
+        editor.commit();
     }
 
     public void setButtonsEnable(boolean enable) {
@@ -124,23 +137,35 @@ public class MyActivity extends Activity {
         edtTextIpAddrChunk1.setText(ipChunkList[0].trim());
         edtTextIpAddrChunk2.setText(ipChunkList[1].trim());
         edtTextIpAddrChunk3.setText(ipChunkList[2].trim());
-        edtTextIpAddrChunk4.setText(ipChunkList[3].trim());  // trim is needed here, because " 1" can not be parsed.
+        edtTextIpAddrChunk4.setText(ipChunkList[3].trim());  // remove spaces for Int parsing
     }
 
+    @SuppressWarnings("UnusedParameters")
     public void onClearButtonClick(View v) {
         setIPAddrToGUI(CLEAR_IP);
         edtTextIpAddrChunk1.requestFocus();
     }
 
+    @SuppressWarnings("UnusedParameters")
     public void onResetButtonClick(View v) {
-        setIPAddrToGUI(DEFAULT_IP);
+        setIPAddrToGUI(getPrefIpAddr(DEFAULT_IP_ADDR_KEY));
     }
 
+    @SuppressWarnings("UnusedParameters")
+    public void onSetAsDefaultButtonClick(View v) {
+        String ipAddr = getIPAddrFromGUI();
+        setPrefIpAddr(DEFAULT_IP_ADDR_KEY, ipAddr);
+        String msg = "Default IPAddr is set to " + ipAddr;
+        Toast toast = Toast.makeText(this.getApplicationContext(), msg, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @SuppressWarnings("UnusedParameters")
     public void onEmitButtonClick(View v) {
         int checkRadioButtonId = cmdOptionsRadioGroup.getCheckedRadioButtonId();
         RadioButton cmdRadioButton = (RadioButton) cmdOptionsRadioGroup.findViewById(checkRadioButtonId);
         int cmdOptionIndex = cmdOptionsRadioGroup.indexOfChild(cmdRadioButton);
-        String msg = "Send to : " + getIPAddr() + " , with cmd : " + cmdOptionIndex;
+        String msg = "Send to : " + getIPAddrFromGUI() + " , with cmd : " + cmdOptionIndex;
 
         Toast toast = Toast.makeText(this.getApplicationContext(), msg, Toast.LENGTH_SHORT);
         toast.show();
@@ -154,7 +179,7 @@ public class MyActivity extends Activity {
         return retVal + origin[origin.length - 1];
     }
 
-    public String getIPAddr() {
+    public String getIPAddrFromGUI() {
         String[] ipAddrChunkList = getIPAddrChunkListFromView();
         return makeString(ipAddrChunkList, ".");
     }
