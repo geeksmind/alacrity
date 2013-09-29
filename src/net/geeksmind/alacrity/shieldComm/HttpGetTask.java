a599c4235c53912e7b1498af5909b4200aac5191
coderh <ren.hao.invictus@gmail.com>
2013-09-29 22:05:10 +0200

    clean and test release

--- src/net/geeksmind/alacrity/shieldComm/HttpGetTask.java
+++ src/net/geeksmind/alacrity/shieldComm/HttpGetTask.java
@@ -1,20 +1,12 @@
 package net.geeksmind.alacrity.shieldComm;
 
-import android.app.ActivityManager;
-import android.content.ComponentName;
 import android.os.AsyncTask;
 import android.util.Log;
-import android.widget.Toast;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
-import net.geeksmind.alacrity.console.consoleActivity;
-import org.apache.http.params.HttpParams;
 
 import java.io.IOException;
-import java.util.List;
 
 /**
  * Author: coderh
@@ -39,7 +31,6 @@ public class HttpGetTask extends AsyncTask<String, Void, String> {
     }
 
     private String getInfo(String url) throws IOException {
-
 
         // Create Request to server and get response
         HttpGet httpget = new HttpGet(url);
